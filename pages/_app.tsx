5d1be5f86a4b6858233a4f6ed0092e951a223469
Sarwang Jain <sarwangjain05@gmail.com>
2021-07-28 04:23:31 +0530

    Configured Nextjs to use react-css-theme-switcher

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
-import "antd/dist/antd.less";
-import "antd/dist/antd.dark.less";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import "../styles/globals.css";
+import { ThemeSwitcherProvider } from "react-css-theme-switcher";
+import "../public/light-theme.css";
+import "../public/dark-theme.css";
+
+const themes = {
+    dark: `/dark-theme.css`,
+    light: `/light-theme.css`,
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <ThemeSwitcherProvider themeMap={themes} defaultTheme="dark">
+            <Component {...pageProps} />;
+        </ThemeSwitcherProvider>
+    );
 }
 
 export default MyApp;
