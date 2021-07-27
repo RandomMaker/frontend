ba6d71fdfad5ecb46665551737fc5fa5eea8ce7b
Sarwang Jain <sarwangjain05@gmail.com>
2021-07-27 09:00:01 +0530

    Project ready for use

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "antd/dist/antd.less";
+import "antd/dist/antd.dark.less";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import "../styles/globals.css";
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Button, DatePicker, Tooltip } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <Head>
-                <title>Next.js</title>
+                <title>Nexand</title>
             </Head>
-            <DatePicker />
-            <Button type="primary">Click me</Button>
+            <div className={styles.present}>
+                <h2>Presenting you</h2>
+                <h1>Nexand - Nextjs + AntDesign</h1>
+                <h4>Everything you ever imagined</h4>
+            </div>
+            <div className={styles.showcomponents}>
+                <DatePicker />
+                <Button type="primary">Click me</Button>
+            </div>
         </div>
     );
 }
